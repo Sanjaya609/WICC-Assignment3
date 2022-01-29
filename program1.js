97cc154e52072a763e515a44fe2726d49d757b37
Sanjaya609 <Sanjayakhatri5@gmail.com>
2022-01-29 20:03:27 +0545

    Assignment Started

---
+++ program1.js
@@ -0,0 +1,6 @@
+const prompt=require('prompt-sync')();
+let string= prompt("Enter a string ");
+let length= string.length;
+let lastChar= string.charAt(length-1);
+console.log(`The length of  ${string} is ${length}`);
+console.log(`The last character of  ${string} is ${lastChar}`);
